1ed82aa6951e4237a973fa2cca8fbea17fc79cd1
x00143663 <culi1602@yahoo.com>
2020-04-21 22:49:32 +0100

    test

---
+++ README.md
@@ -0,0 +1,171 @@
+# Google Maps Safest Route
+This repository contains overview documentation and project management materials
+for the *Google Maps Safest Route* product.
+#### In this README
+- [Product Overview](#product-overview)
+- [Architecture Description](#architecture-description)
+- [Developer Guide](#developer-guide)
+## Product Overview
+*Google Maps Safest Route* is a web application that helps users visualize dangerous spots on the map and to display the distance between a given address and the respective dangerous spot. 
+Users can find more information about that spots by clicking on the marker.
+## Architecture Description
+### Front Web aplication 
+This is the interface betwen the user and the application. The user can look over the map, can zoom in and out of the map and if the user provides an address the application will generate a list with the dangerous spots and distance to that point from the provided address.
+Each dangerous point displaied on the map can display more information like type of the crime, address, description, url from where the information was collected, date when the event took place, geographical coordinates and a picture of the place.
+### Places info Database
+That database will provide information about dangerous points and the web application will use that database.
+That database will be updated with new informations. 
+### Raw data Database
+That database will be used to store raw data collected by the scraper script and will be used by a Natural Languade processing application to generate dangerous data points record.
+
+### Google Maps Platform APIs
+We have used Sendgrid API to send quotes to our users.
+- [Find out more about Google Maps Platform APIs](https://developers.google.com/maps/documentation/javascript/tutorial)
+### Firebase Realtime Database
+The Firebase Realtime Database is a cloud-hosted database. Data is stored under the JSON format and synchronized in realtime to every connected client. When you build cross-platform apps with our iOS, Android, and JavaScript SDKs, all of your clients share one Realtime Database instance and automatically receive updates with the newest data.
+- [Find out more about Realtime Database](https://firebase.google.com/docs/database)
+### Google Cloud 
+Google Cloud Platform, offered by Google, is a suite of cloud computing services that runs on the same infrastructure that Google uses internally for its end-user products, such as Google Search, Gmail and YouTube.
+Google Cloud Platform provides infrastructure as a service, platform as a service, and serverless computing environments.
+Cloud computing platforms, like Google Cloud, tend to be less expensive and more secure, reliable, and flexible than on-premises servers. With the cloud, equipment downtime due to maintenance, theft, or damage is almost non-existent. You can scale your compute and storage resources—up or down—almost instantly when your needs change on Google Cloud. Also, you typically pay only for the services you use, which provides a level of convenience and cost-control that's almost impossible to achieve with on-site infrastructure.
+In this project we utilize Google Cloud Storage (bucket) to deploy the static web application, Firebase Realtime Database to store data.
+### IBM Github
+For project development I use IBM Github for tracking changes in source code during development.
+IBM Github provides hosting for software development version control using Git.
+Git is a distributed version-control system for tracking changes in source code during software development. It is designed for coordinating work among programmers, but it can be used to track changes in any set of files. Its goals include speed, data integrity, and support for distributed, non-linear workflows. 
+- [Find out more about Git](https://git-scm.com/)
+### Travis CI / CD
+Continuous Integration / Continuous Deployment will be used to support fast deployment
+of new features to production, as they are developed. We have a pipeline that runs from IBM Git into Google Cloud straight to production.
+Travis pipeline will be triggered when a new code version is committed.
+-   Travis file 
+ ```code
+ language: python
+python:
+  - "3.6"
+cache: pip
+install:
+  - pip install -r requirements.txt
+script:
+  - python scripts/searchandscrape.py
+  - python scripts/UploadToRealtimePy.py
+deploy:
+  provider: gcs
+  access_key_id: ${access_key_id}
+  secret_access_key: ${secret_access_key}
+  bucket: culi-data-loc
+  cache_control: "no-cache"
+  local-dir: sourcecode
+  on:
+    all_branches: true
+    repo: ITT-Projects/Google-Maps-Safest-Route
+ ```
+
+- [Find out more about Travis](https://travis-ci.org/)
+
+### Architecture
+![Architectue](docs/diagram.PNG)
+
+### Environments
+We have used truck based development so we just have a *production* system.
+
+## Prerequisites
+This project **requires** prerequisites in order to run.
+- GitHub Account
+- Google Cloud Account
+- Travis Account
+### Overview
+Google Maps Safest Route application consists two main actions:
+ - Retrieve and display information.
+ 
+On this project I will cover the following: searching the internet, scraping raw data and finally, displaying the data on the map.
+As a future activity, raw data processing can be considered in order to update the database with the latest events.
+For the development of the application we used the following technologies.
+- for the web application I used HTML, JavaScript and Google Maps JavaScript API
+- [Find out more about HTML](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+- [Find out more about JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+- [Find out more about Google Maps JavaScript API](https://developers.google.com/maps/documentation/javascript/tutorial)
+- for the web scraper scripts and data transfer scrips I used Python3 and the following library - Beautiful Soup, Pandas, Firebase
+- [Find out more about Python 3](https://www.python.org/)
+- [Find out more about Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
+- [Find out more about Pandas](https://pandas.pydata.org/) 
+- [Find out more about Firebase](https://pypi.org/project/python-firebase/) 
+
+
+### Display a Google map centered on Dublin, Ireland
+```code
+   <!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Map</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 53.349805, lng: -6.260255},
+          zoom: 8
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+    async defer></script>
+  </body>
+</html>
+```
+### Script to search on internet for crime data and colect raw data and store in Realtime Database
+```code
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+from googlesearch import search
+from firebase import firebase
+
+# fix .async issue from firebase library https://www.youtube.com/watch?v=TiMACTNbNl8
+#tutorial database  https://codeloop.org/python-firebase-real-time-database/
+firebase = firebase.FirebaseApplication('https://ny-sub-sta-articles.firebaseio.com/', None)
+# to search
+query = "irishtimes Crime in Dublin"
+url = []
+for j in search(query, tld="com", num=10, stop=10, pause=2):
+    url.append(j)
+print (url)
+for my_url in url:
+    try:
+        uClient = uReq(my_url)
+        page_html = uClient.read()
+        uClient.close()
+        # html parsing
+        page_soup = soup(page_html, "html.parser")
+        time = page_soup.find("time").get_text()
+        load = page_soup.find("div", {"class": "article_bodycopy"})
+        items = load.find_all("p", {"class": "no_name"})
+        text = [item.get_text() for item in items]
+        data = {'time': time,
+                'text': text
+                }
+        result = firebase.post('/articles/', data )
+    except:
+        pass
+
+#result2 = firebase.get('/articles/', '')
+#print(result2)
+```
+### To try this project
+- [Google Maps Safest Route](http://storage.googleapis.com/culi-data-loc/index.html) 
---
+++ sourcecode/app.js
@@ -0,0 +1,351 @@
+/*
+ * Copyright 2017 Google Inc. All rights reserved.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+// Style credit: https://snazzymaps.com/style/1/pale-dawn
+const mapStyle = [{
+  'featureType': 'administrative',
+  'elementType': 'all',
+  'stylers': [{
+    'visibility': 'on',
+  },
+  {
+    'lightness': 33,
+  },
+  ],
+},
+{
+  'featureType': 'landscape',
+  'elementType': 'all',
+  'stylers': [{
+    'color': '#f2e5d4',
+  }],
+},
+{
+  'featureType': 'poi.park',
+  'elementType': 'geometry',
+  'stylers': [{
+    'color': '#c5dac6',
+  }],
+},
+{
+  'featureType': 'poi.park',
+  'elementType': 'labels',
+  'stylers': [{
+    'visibility': 'on',
+  },
+  {
+    'lightness': 20,
+  },
+  ],
+},
+{
+  'featureType': 'road',
+  'elementType': 'all',
+  'stylers': [{
+    'lightness': 20,
+  }],
+},
+{
+  'featureType': 'road.highway',
+  'elementType': 'geometry',
+  'stylers': [{
+    'color': '#c5c6c6',
+  }],
+},
+{
+  'featureType': 'road.arterial',
+  'elementType': 'geometry',
+  'stylers': [{
+    'color': '#e4d7c6',
+  }],
+},
+{
+  'featureType': 'road.local',
+  'elementType': 'geometry',
+  'stylers': [{
+    'color': '#fbfaf7',
+  }],
+},
+{
+  'featureType': 'water',
+  'elementType': 'all',
+  'stylers': [{
+    'visibility': 'on',
+  },
+  {
+    'color': '#acbcc9',
+  },
+  ],
+},
+];
+
+// Escapes HTML characters in a template literal string, to prevent XSS.
+// See https://www.owasp.org/index.php/XSS_%28Cross_Site_Scripting%29_Prevention_Cheat_Sheet#RULE_.231_-_HTML_Escape_Before_Inserting_Untrusted_Data_into_HTML_Element_Content
+function sanitizeHTML(strings) {
+  const entities = { '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', '\'': '&#39;' };
+  let result = strings[0];
+  for (let i = 1; i < arguments.length; i++) {
+    result += String(arguments[i]).replace(/[&<>'"]/g, (char) => {
+      return entities[char];
+    });
+    result += strings[i];
+  }
+  return result;
+}
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBkYlruEanq2C3DmywfA4-mEE7JpO276ZE",
+  authDomain: "ny-sub-sta.firebaseapp.com",
+  databaseURL: "https://ny-sub-sta.firebaseio.com",
+  projectId: "ny-sub-sta",
+  storageBucket: "ny-sub-sta.appspot.com",
+  messagingSenderId: "602490105733",
+  appId: "1:602490105733:web:be91c1cc1daa0b03d1deec"
+};
+// Initialize Firebase
+// Create the map.
+initMap = async () => { 
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: { lat: 53.349816, lng: -6.260257 },
+    styles: mapStyle,
+  });
+ //datastore used for test
+  //const DATA = 'https://api.jsonbin.io/b/5e4ef807d3c2f35597f4c1f8';
+
+  var DATA = 'https://ny-sub-sta.firebaseio.com/data.json';
+
+  
+  // Load the places GeoJSON onto the map.
+  map.data.loadGeoJson(DATA, { idPropertyName: 'placeid' });
+
+  // Define the custom marker icons, using the place's "category".
+  map.data.setStyle((feature) => {
+    return {
+      icon: {
+        url: `img/icon_${feature.getProperty('category')}.png`,
+        scaledSize: new google.maps.Size(32, 32),
+      },
+    };
+  });
+
+  const apiKey2 = 'AIzaSyBxT9pyvtTWkBOWHfzjynZrheCuhJ13Au4';
+  const infoWindow = new google.maps.InfoWindow();
+
+  // Show the information for a place when its marker is clicked.
+  map.data.addListener('click', (event) => {
+
+    const name = event.feature.getProperty('name');
+    const description = event.feature.getProperty('description');
+    const date = event.feature.getProperty('date');
+    const url = event.feature.getProperty('url');
+    const position = event.feature.getGeometry().get();
+    const content = sanitizeHTML`
+      <div style="margin-left:20px; margin-bottom:20px;">
+        <h2>${name}</h2><p>${description}</p>
+        <p><b>Incident date:</b> ${date}<br/><b>URL:</b> ${url}<br/><b>Lat.:</b> ${position.lat()}<br/><b>Long.:</b> ${position.lng()}</p>
+        <p><img src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location=${position.lat()},${position.lng()}&key=${apiKey2}"></p>
+      </div>
+      `;
+
+    infoWindow.setContent(content);
+    infoWindow.setPosition(position);
+    infoWindow.setOptions({ pixelOffset: new google.maps.Size(0, -30) });
+    infoWindow.open(map);
+  });
+
+  // Build and add the search bar
+  const card = document.createElement('div');
+  const titleBar = document.createElement('div');
+  const title = document.createElement('div');
+  const container = document.createElement('div');
+  const input = document.createElement('input');
+  const options = {
+    types: ['address'],
+    componentRestrictions: { country: 'irl' },
+  };
+
+  card.setAttribute('id', 'pac-card');
+  title.setAttribute('id', 'title');
+  title.textContent = 'Detect crime places near your position';
+  titleBar.appendChild(title);
+  container.setAttribute('id', 'pac-container');
+  input.setAttribute('id', 'pac-input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', 'Enter your position');
+  container.appendChild(input);
+  card.appendChild(titleBar);
+  card.appendChild(container);
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+  // Make the search bar into a Places Autocomplete search bar and select
+  // which detail fields should be returned about the place that
+  // the user selects from the suggestions.
+  const autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  autocomplete.setFields(
+    ['address_components', 'geometry', 'name']);
+
+  // Set the origin point when the user selects an address
+  const originMarker = new google.maps.Marker({ map: map });
+  originMarker.setVisible(false);
+  let originLocation = map.getCenter();
+
+  autocomplete.addListener('place_changed', async () => {
+    originMarker.setVisible(false);
+    originLocation = map.getCenter();
+    const place = autocomplete.getPlace();
+
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert('No address available for input: \'' + place.name + '\'');
+      return;
+    }
+
+    // Recenter the map to the selected address
+    originLocation = place.geometry.location;
+    map.setCenter(originLocation);
+    map.setZoom(15);
+    console.log(place);
+
+    originMarker.setPosition(originLocation);
+    originMarker.setVisible(true);
+
+    // Use the selected address as the origin to calculate distances
+    // to each of the place locations
+    const rankedPlaces = await calculateDistances(map.data, originLocation);
+    showPlacesList(map.data, rankedPlaces);
+
+    return;
+  });
+}
+
+/**
+ * Use Distance Matrix API to calculate distance from origin to each place.
+ * @param {google.maps.Data} data The geospatial data object layer for the map
+ * @param {google.maps.LatLng} origin Geographical coordinates in latitude
+ * and longitude
+ * @return {Promise<object[]>} n Promise fulfilled by an array of objects with
+ * a distanceText, distanceVal, and placeid property, sorted ascending
+ * by distanceVal.
+ */
+async function calculateDistances(data, origin) {
+  const places = [];
+  const destinations = [];
+
+  // Build parallel arrays for the place IDs and destinations
+  data.forEach((place) => {
+    const placeNum = place.getProperty('placeid');
+    const placeLoc = place.getGeometry().get();
+
+    places.push(placeNum);
+    destinations.push(placeLoc);
+  });
+
+  // Retrieve the distances of each place from the origin
+  // The returned list will be in the same order as the destinations list
+  const service = new google.maps.DistanceMatrixService();
+  const getDistanceMatrix =
+    (service, parameters) => new Promise((resolve, reject) => {
+      service.getDistanceMatrix(parameters, (response, status) => {
+        if (status != google.maps.DistanceMatrixStatus.OK) {
+          reject(response);
+        } else {
+          const distances = [];
+          const results = response.rows[0].elements;
+          for (let j = 0; j < results.length; j++) {
+            const element = results[j];
+            const distanceText = element.distance.text;
+            const distanceVal = element.distance.value;
+            const distanceObject = {
+              placeid: places[j],
+              distanceText: distanceText,
+              distanceVal: distanceVal,
+            };
+            distances.push(distanceObject);
+          }
+
+          resolve(distances);
+        }
+      });
+    });
+  //DRIVING / TRANSIT / BICYCLING /WALKING
+  const distancesList = await getDistanceMatrix(service, {
+    origins: [origin],
+    destinations: destinations,
+    travelMode: 'WALKING',
+    unitSystem: google.maps.UnitSystem.METRIC,
+  });
+
+  distancesList.sort((first, second) => {
+    return first.distanceVal - second.distanceVal;
+  });
+
+  return distancesList;
+}
+
+/**
+ * Build the content of the side panel from the sorted list of places
+ * and display it.
+ * @param {google.maps.Data} data The geospatial data object layer for the map
+ * @param {object[]} places An array of objects with a distanceText,
+ * distanceVal, and placeid property.
+ */
+function showPlacesList(data, places) {
+  if (places.length == 0) {
+    console.log('empty places');
+    return;
+  }
+
+  let panel = document.createElement('div');
+  // If the panel already exists, use it. Else, create it and add to the page.
+  if (document.getElementById('panel')) {
+    panel = document.getElementById('panel');
+    // If panel is already open, close it
+    if (panel.classList.contains('open')) {
+      panel.classList.remove('open');
+    }
+  } else {
+    panel.setAttribute('id', 'panel');
+    const body = document.body;
+    body.insertBefore(panel, body.childNodes[0]);
+  }
+
+
+  // Clear the previous details
+  while (panel.lastChild) {
+    panel.removeChild(panel.lastChild);
+  }
+
+  places.forEach((place) => {
+    // Add place details with text formatting
+    const name = document.createElement('p');
+    name.classList.add('place');
+    const currentPlace = data.getFeatureById(place.placeid);
+    name.textContent = currentPlace.getProperty('name');
+    panel.appendChild(name);
+    const distanceText = document.createElement('p');
+    distanceText.classList.add('distanceText');
+    distanceText.textContent = place.distanceText;
+    panel.appendChild(distanceText);
+  });
+
+  // Open the panel xx
+  panel.classList.add('open');
+
+  return;
+}
